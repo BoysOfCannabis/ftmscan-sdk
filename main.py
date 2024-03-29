1afae5a5593f48376828e2fa88894f677b2e3140
OmidSh <omidsha@pm.me>
2024-03-29 18:49:26 +0330

    init basic design of classes

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import aiohttp
+import abc
 
+class BlockExplorer(abc.ABC):
+    def __init__(self, API_KEY):
+        self.API_KEY = API_KEY
+    
+class FTMExplorer(BlockExplorer):
+    def get_transactions(self):
+        ...
+        
